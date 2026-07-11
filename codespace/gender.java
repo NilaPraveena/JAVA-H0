e90910ad8b1c2ab4650d32201288f35c33ed80cf
NilaPraveena <chinnunila368@gmail.com>
2026-07-11 16:18:54 +0530

    Add files via upload

---
+++ codespace/gender.java
@@ -0,0 +1,8 @@
+public class gender{
+public static void main(String args[]){
+String gender="F";
+char gender2='M';
+System.out.println("gender="+gender);
+System.out.println("gender2="+gender2);
+}
+}
