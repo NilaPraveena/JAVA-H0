12f7f9cb8591de9c0256d3715c5ec77e3488427c
NilaPraveena <chinnunila368@gmail.com>
2026-08-04 15:20:22 +0530

    Add files via upload

---
+++ codespace/ScopeEx.java
@@ -0,0 +1,16 @@
+class ScopeEx{
+public static void main(String args[]){
+String name="Praveena";
+int age = 18;
+System.out.println(name);
+System.out.println(age+"years old");
+if(age>=18){
+String status="Allowed";
+System.out.println(status);
+}
+else{
+String status = "Not allowed";
+System.out.println(status);
+}
+}
+}
