47c1c31054bb530617f4430609c07b3310232ddc
NilaPraveena <chinnunila368@gmail.com>
2026-08-25 16:14:13 +0530

    Add files via upload

---
+++ codespace/PrimitiveDataTypes.java
@@ -0,0 +1,20 @@
+class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 10000000000L;
+        float f = 10.5f;
+        double d = 99.99;
+        char c = 'A';
+        boolean bool = true;
+        System.out.println("byte    : " + b);
+        System.out.println("short   : " + s);
+        System.out.println("int     : " + i);
+        System.out.println("long    : " + l);
+        System.out.println("float   : " + f);
+        System.out.println("double  : " + d);
+        System.out.println("char    : " + c);
+        System.out.println("boolean : " + bool);
+    }
+}
